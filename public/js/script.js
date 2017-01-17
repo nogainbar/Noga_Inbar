ade5f04b39a9aefe4589030efaf5ea656dccf91e
Noga <noga.inbar@gmail.com>
2017-01-17 00:04:23 +0000

    added transition to navbar but still needs love

--- public/js/script.js
+++ public/js/script.js
@@ -3,6 +3,19 @@ $(document).ready(function() {
 
   console.log("hire me! im fun to work with and I love beautiful things! :)");
 
+  $(window).scroll(function() {
+
+         var height = $('.wrapperNav').height();
+         var scrollTop = $(window).scrollTop();
+
+         if (scrollTop >= height - 100) {
+             $('.nav').addClass('solid-nav');
+             console.log('yo!');
+         } else {
+             $('.nav').removeClass('solid-nav');
+         }
+
+     });
   // $(".fade").hide(0).delay(500).fadeIn(3000);
   $('.fadeOnLoad').fadeIn('slow');
 
