f3b46ce0c96b0aa7f97c8bbed02c82575fd8784e
Ivan Gonzalez <ivangonzalez@rocketmail.com>
2017-08-27 18:56:14 +0100

    Uglify css

--- postcss.config.js
+++ postcss.config.js
@@ -3,9 +3,9 @@ module.exports = {
     require('postcss-import')(),
     require('autoprefixer')(),
     require('postcss-custom-media')(),
-    // require('postcss-clean')(),
     require('postcss-custom-properties')(),
     require('postcss-nested')(),
-    require('postcss-simple-vars')()
+    require('postcss-simple-vars')(),
+    require('postcss-clean')()
   ]
 }
