5ea55e8c431aa91a007eea8849a1334dc4bc2798
Noga <noga.inbar@gmail.com>
2017-12-28 16:45:21 +0000

    added go to top button on landing

--- public/js/script.js
+++ public/js/script.js
@@ -4,6 +4,7 @@ $(document).ready(function() {
   console.log("Contact me if you want to chat! im fun to work with, I love beautiful things and I am always up for an interesting collaboration! :)");
   console.log("noga.inbar@gmail.com");
 
+  // nav colour change
   $(window).scroll(function() {
 
          var height = $('.wrapperNav').height();
@@ -19,6 +20,9 @@ $(document).ready(function() {
          }
 
      });
+
+    //  fade effect of projects
+
   // $(".fade").hide(0).delay(500).fadeIn(3000);
   $('.fadeOnLoad').fadeIn('slow');
 
@@ -28,4 +32,18 @@ $(document).ready(function() {
           easing: 'ease-in-sine',
           delay: 100,
         });
+
+    // scroll to top button
+    $(window).scroll(function(){
+        if ($(this).scrollTop() > 100) {
+            $('#scroll').fadeIn();
+        } else {
+            $('#scroll').fadeOut();
+        }
+    });
+    $('#scroll').click(function(){
+        $("html, body").animate({ scrollTop: 0 }, 600);
+        return false;
+    });
+
 });
